e09781a9db657966c877b49a3d451866eddc9f39
JuhDeFreita <julia.carvalho@gec.inatel.br>
2026-03-30 00:31:19 -0300

    refactor: code corrections

--- include/mqtt_config.h
+++ include/mqtt_config.h
@@ -1,9 +1,9 @@
 #ifndef MQTT_CONFIG_H
 #define MQTT_CONFIG_H
 
-#define MQTT_BROKER_URI   "mqtt://localhost:1883"
-#define MQTT_TOPIC_PUBLISH       "tcc/{$DEVICE_ID}/{$SENSOR_TYPE}"
-#define MQTT_TOPIC_SUBSCRIBE        "tcc/config/{$DEVICE_ID}/set"
+#define MQTT_BROKER_URI   "mqtt://broker.emqx.io:1883"
+#define MQTT_TOPIC_PUBLISH       "health/DEVICE_ID/signal/SENSOR_TYPE"
+#define MQTT_TOPIC_SUBSCRIBE        "health/DEVICE_ID/config/CONFIG_TYPE"
 #define MQTT_CLIENT_ID    "esp32_client"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
 #include "wifi.h"
 #include "mqtt.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main() {
-  wifi_init();
-  mqtt_init();
+void app_main(void)
+{
+    wifi_init();
+    wifi_start();
 
-  for(int i = 0; i < 100; i++) {
-    printf("\nOlá Mundo!\n");
-    mqtt_publish_message("Olá Mundo!");
-  }
+    mqtt_init();
+
+    while (1) {
+        mqtt_publish_message("Hello ESP32");
+        vTaskDelay(pdMS_TO_TICKS(5000)); // espera 5s
+    }
 }
